1967b3ec4ecb874d954f1a3c6e63f06d29feffe5
Giovanni Rizzardo <giovannirizzardo@MacBook-Pro-de-Giovanni.local>
2021-03-23 17:48:24 -0300

    inserido o coracao como botao para o texto central

--- app/src/main/java/br/com/applicationmfy/MainActivity.kt
+++ app/src/main/java/br/com/applicationmfy/MainActivity.kt
@@ -1,11 +1,29 @@
 package br.com.applicationmfy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import xyz.hanks.library.bang.SmallBangView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<SmallBangView>(R.id.imageViewAnimation)
+
+        imageView.setOnClickListener {
+            if (imageView.isSelected) {
+                imageView.isSelected = false
+//                imageView.likeAnimation()
+            } else {
+                // if not selected only
+                // then show animation.
+                imageView.isSelected = true
+                imageView.likeAnimation()
+            }
+        }
     }
 }
